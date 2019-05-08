f9b76f869d024f1b5c434351ace919ca32020ec7
Wynwood <marcus.wynwood@education.tas.gov.au>
2019-05-08 16:10:55 +1000

    Add project files.

---
+++ KeyPresser/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace KeyPresser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.aboutKeyPresserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutKeyPresserToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(174, 48);
+            // 
+            // aboutKeyPresserToolStripMenuItem
+            // 
+            this.aboutKeyPresserToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutKeyPresserToolStripMenuItem.Name = "aboutKeyPresserToolStripMenuItem";
+            this.aboutKeyPresserToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.aboutKeyPresserToolStripMenuItem.Text = "About KeyPresser";
+            this.aboutKeyPresserToolStripMenuItem.Click += new System.EventHandler(this.aboutKeyPresserToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(183, 68);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutKeyPresserToolStripMenuItem;
+    }
+}
+
---
+++ KeyPresser/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace KeyPresser
+{
+    public partial class Form1 : Form
+    {
+        // Lets you do key presses
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        // Hides it from the ALT-TAB menu
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr window, int index, int value);
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr window, int index);
+
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_TOOLWINDOW = 0x00000080;
+        const int WS_EX_APPWINDOW = 0x00040000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1.Text = Application.ProductName;
+            this.Text = Application.ProductName;
+            timer1.Interval = 59000; // 59 Seconds
+            timer1.Start();
+
+            // Hides it from the ALT-TAB menu
+            int windowStyle = GetWindowLong(Handle, GWL_EXSTYLE);
+            SetWindowLong(Handle, GWL_EXSTYLE, windowStyle | WS_EX_TOOLWINDOW);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Press 'Reserved' key
+            keybd_event(0xFC, 0x45, 0, 0);
+            //Console.Beep();
+
+            // Press Scroll Lock again
+            //keybd_event(0x91, 0x45, 0, 0);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false; // Fixes a bug where the icon stays after it's closed
+            Application.Exit();
+        }
+
+        private void aboutKeyPresserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+            about.StartPosition = FormStartPosition.CenterScreen;
+            about.ShowDialog();
+        }
+    }
+}
